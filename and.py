d53d2bd56f7053f6a8e2f3181d194e6a9991dd5f
activevj <uservjkumar@gmail.com>
2021-09-27 10:37:57 +0530

    second commit

--- and.py
+++ and.py
@@ -1,7 +1,7 @@
 from utils.model import Perceptron
 import pandas as pd
 import numpy as np
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_plot, save_model
 
 
 
@@ -22,3 +22,6 @@ model = Perceptron(eta=ETA, epochs=EPOCHS)
 model.fit(X, y)
 
 _ = model.total_loss()
+
+save_model(model,filename="and.model")
+save_plot(df,"and.png",model)
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -20,7 +20,7 @@ def prepare_data(df):
   return X, y
 
 
-  def save_model(model, filename):
+def save_model(model, filename):
     model_dir = "models"
     os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
     filePath = os.path.join(model_dir, filename) # model/filename
