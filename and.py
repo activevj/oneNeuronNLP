c2e28247bd1ff857786f2204b541fa8abf8ee5a8
activevj <uservjkumar@gmail.com>
2021-09-27 10:59:09 +0530

    code organized

--- and.py
+++ and.py
@@ -1,27 +1,30 @@
+from matplotlib.pyplot import plot
 from utils.model import Perceptron
 import pandas as pd
 import numpy as np
 from utils.all_utils import prepare_data, save_plot, save_model
 
 
+def main(data, eta ,epochs,filename,plotfilename):
 
-AND = {
+    df = pd.DataFrame(data)
+    X,y = prepare_data(df)
+
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
+
+    _ = model.total_loss() ##dummy variable
+
+    save_model(model,filename=filename)
+    save_plot(df,plotfilename,model)
+
+if __name__ == '__main__':  ## << entry point
+    AND = {
     "x1": [0,0,1,1],
     "x2": [0,1,0,1],
     "y": [0,0,0,1],
-}
+    }
+    ETA=0.3
+    EPOCHS=10
 
-df = pd.DataFrame(AND)
-
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-
-save_model(model,filename="and.model")
-save_plot(df,"and.png",model)
+    main(AND,ETA,EPOCHS,filename="and.model",plotfilename="and.png")
--- or.py
+++ or.py
@@ -3,25 +3,26 @@ import pandas as pd
 import numpy as np
 from utils.all_utils import prepare_data, save_plot, save_model
 
+def main(data, eta ,epochs,filename,plotfilename):
 
+    df = pd.DataFrame(data)
+    X,y = prepare_data(df)
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
 
-df_or = pd.DataFrame(OR)
+    _ = model.total_loss() ##dummy variable
 
-X,y = prepare_data(df_or)
+    save_model(model,filename=filename)
+    save_plot(df,plotfilename,model)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+if __name__ == '__main__':  ## << entry point
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+    ETA=0.3
+    EPOCHS=10
 
-model_or = Perceptron(eta=ETA, epochs=EPOCHS)
-model_or.fit(X, y)
-
-_ = model_or.total_loss()
-
-save_model(model_or,filename="or.model")
-save_plot(df_or,"or.png",model_or)
+    main(OR,ETA,EPOCHS,filename="or.model",plotfilename="or.png")
