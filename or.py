8fee8db2637f058913f485517742706795081f56
activevj <uservjkumar@gmail.com>
2021-09-30 20:03:30 +0530

    Update string

--- or.py
+++ or.py
@@ -9,10 +9,19 @@ from utils.model import Perceptron
 import pandas as pd
 import numpy as np
 from utils.all_utils import prepare_data, save_plot, save_model
+import logging
+import os
+
+logging_str = "[%(asctime)s - %(levelname)s: %(module)s] %(message)s"
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str,
+filemode='a')
 
 def main(data, eta ,epochs,filename,plotfilename):
 
     df = pd.DataFrame(data)
+    logging.info(f"This is actual dataframe {df}")
     X,y = prepare_data(df)
 
     model = Perceptron(eta=ETA, epochs=EPOCHS)
@@ -30,6 +39,11 @@ if __name__ == '__main__':  ## << entry point
         "y": [0,1,1,1],
     }
     ETA=0.3
-    EPOCHS=10
-
-    main(OR,ETA,EPOCHS,filename="or.model",plotfilename="or.png")
+    EPOCHS=100
+    try:
+        logging.info(">>>>>>>>>>>>>>>> Starting Training Here >>>>>>>>>>>>>>>>>>>")
+        main(OR,ETA,EPOCHS,filename="or.model",plotfilename="or.png")
+        logging.info(">>>>>>>>>>>>>>>> training done successfully >>>>>>>>>>>>>>>>>>>\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,6 +4,8 @@ import pandas as pd
 import joblib # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
+import logging
+
 plt.style.use("fivethirtyeight")
 
 
@@ -21,6 +23,7 @@ def prepare_data(df):
   Returns:
       [tuple]: It returns the tuple
   """
+  logging.info("Preparing the data by segregating the data")
   X = df.drop("y", axis=1)
 
   y = df["y"]
@@ -35,10 +38,12 @@ def save_model(model, filename):
       model (Python object containing the model to save): Model to save_model
       filename (str): used to generate the location to save the model
   """
+  logging.info("Saving the trained model")
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
+  logging.info(f"saved the trained model {filePath}")
 
 
 def save_plot(df, file_name, model):
@@ -50,6 +55,7 @@ def save_plot(df, file_name, model):
       model (python object): Python object describing the plot to save
   """
   def _create_base_plot(df):
+    logging.info("creating base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -57,6 +63,7 @@ def save_plot(df, file_name, model):
     figure.set_size_inches(10, 8)
 
   def _plot_decision_regions(X, y, classfier, resolution=0.02):
+    logging.info("plotting the decision regions")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -68,8 +75,7 @@ def save_plot(df, file_name, model):
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
                            np.arange(x2_min, x2_max, resolution))
-    print(xx1)
-    print(xx1.ravel())
+
     Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
@@ -89,3 +95,4 @@ def save_plot(df, file_name, model):
   os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   plotPath = os.path.join(plot_dir, file_name) # model/filename
   plt.savefig(plotPath)
+  logging.info(f"Saving the plots at {plotPath}")
